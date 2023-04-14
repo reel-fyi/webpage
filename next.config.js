6a58a7d8d97b738902fdb6408aa3fb7122629e26
Yesh Chandiramani <yesh0907@hotmail.com>
2023-04-13 20:25:08 -0700

    add redirect and favicon

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://reel.fyi/',
+        permanent: true,
+      },
+    ]
+  }
 }
