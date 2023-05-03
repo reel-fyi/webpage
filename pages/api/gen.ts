6b19f8e7cd8f5775b9921643bb6db1b7d15cffe6
Yesh Chandiramani <yesh0907@hotmail.com>
2023-05-02 17:18:20 -0700

    add open ai api

--- pages/api/gen.ts
+++ pages/api/gen.ts
@@ -1,11 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import Cors from 'cors';
-import e from "cors";
+import Cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
 
 // Initializing the cors middleware
 const cors = Cors({
-  methods: ['POST'],
-  origin: '*',
+  methods: ["POST"],
+  origin: "*",
 });
 
 function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
@@ -18,6 +18,12 @@ function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
     });
   });
 }
+
+// OpenAI initialization
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 type Data = {
   message: string;
@@ -48,7 +54,18 @@ export default async function handler(
       console.log(
         `${user.id}: ${user.name} requested a connection message for ${conn.name} - ${conn.info}`
       );
-      const message = `Hi ${conn.name},\n\n${user.name} has requested to connect with you. They like this experience you have: ${conn.info}`;
+      const prompt = `Write a LinkedIn connection request message according to the following requirements. Personalize it based on the sender and recipient info. Keep it short, concise and in plain English. Focus on the recipient's primary need of connecting with someone they want to learn more about in terms of a role and/or a specific company and include a clear call to action in a short phone/online call AKA a coffee chat or information interview.\nSender info: name, ${user.name}; experience, ${user.bio}\nRecipient info: name, ${conn.name}; profile information, ${conn.info}`;
+      const genRes = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+      const message = genRes.data.choices[0].text?.trim() || "";
+      // const message = `Hi ${conn.name},\n\n${user.name} has requested to connect with you. They like this experience you have: ${conn.info}`;
       res.status(200).json({ message });
     } else {
       res.status(400).json({ message: "Invalid user data" });
